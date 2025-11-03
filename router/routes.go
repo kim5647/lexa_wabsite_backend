cc6a544e00691c5ef36fa015d19770283da59883
kim5647 <stepanv2005x@gmail.com>
2025-11-03 15:40:59 +0300

    edit router

--- router/routes.go
+++ router/routes.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AddUserRoutes(rg *gin.RouterGroup, handlers.UserHandler){
+type Dependencies struct {
+	UserHandler *handlers.UserHandler
+}
 
+func RegisterRoutes(r *gin.Engine, deps *Dependencies) {
+	// Создаем группу для пользователя
+	userGroup := r.Group("/users")
+	{
+		userGroup.GET("/", deps.UserHandler.GetUsers) // Убедитесь, что GetUsers - метод *UserHandler
+		userGroup.POST("/", deps.UserHandler.CreateUser)
+		userGroup.GET("/:id", deps.UserHandler.GetUserByID)
+	}
 }
